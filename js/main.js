22078d841ca47f983758e66341d6efe9b9ffde07
Mark Anjou <markanjou@yahoo.com>
2018-11-17 15:33:39 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$("#menu-button").click(function() {
+	$("#menu-button").toggleClass("menu-open");
+	$("#menu").toggleClass("menu-collapsed");
+	$("#menu ul li").toggleClass("hidden animated fadeInLeft");
+});
